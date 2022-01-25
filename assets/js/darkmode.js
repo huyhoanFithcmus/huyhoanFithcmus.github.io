da12a61f024367c3e8576ee9f992fb9e1e75bcc5
huyhoan652002 <huyhoan652002@gmail.com>
2022-01-25 18:29:41 +0700

    Add files via upload

---
+++ assets/js/darkmode.js
@@ -0,0 +1,70 @@
+function darkmode()
+{
+    // darkmode pageY
+    var h1Elements = document.getElementsByClassName("l-header");
+
+    for(var i = 0; i < h1Elements.length; i++) {
+    h1Elements[i].style.backgroundColor = "#322E2E";
+    }
+
+    var navlink = document.getElementsByClassName("nav__link");
+
+    for(var i = 0; i < navlink.length; i++) {
+        navlink[i].style.color = "white";
+    }
+
+    var navlink = document.getElementsByClassName("nav__logo");
+
+    for(var i = 0; i < navlink.length; i++) {
+        navlink[i].style.color = "white";
+    }
+
+
+    var bx = document.getElementsByClassName("bx");
+
+    for(var i = 0; i < bx.length; i++) {
+        bx[i].style.color = "white";
+    }
+
+    var h1Elements = document.getElementsByTagName("h1");
+
+    for(var i = 0; i < h1Elements.length; i++) {
+    h1Elements[i].style.color = "white";
+    }
+
+    var h1Elements = document.getElementsByTagName("h2");
+
+    for(var i = 0; i < h1Elements.length; i++) {
+    h1Elements[i].style.color = "white";
+    }
+
+    var h1Elements = document.getElementsByTagName("p");
+
+    for(var i = 0; i < h1Elements.length; i++) {
+    h1Elements[i].style.color = "white";
+    }
+
+    var h1Elements = document.getElementsByTagName("span");
+
+    for(var i = 0; i < h1Elements.length; i++) {
+    h1Elements[i].style.color = "white";
+    }
+
+    var h1Elements = document.getElementsByClassName("skills__data");
+
+    for(var i = 0; i < h1Elements.length; i++) {
+    h1Elements[i].style.backgroundColor = "#449744";
+    }
+
+    var h1Elements = document.getElementsByClassName("darkmode");
+
+    for(var i = 0; i < h1Elements.length; i++) {
+    h1Elements[i].style.backgroundColor = "#449744";
+    }
+
+    var h1Elements = document.getElementsByClassName("l-main");
+
+    for(var i = 0; i < h1Elements.length; i++) {
+    h1Elements[i].style.backgroundColor = "black";
+    }
+}
